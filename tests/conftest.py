250c8085a7d5b7be1770645c545fd2e5679b59e5
Sabirin Mohamed <sabirin.gca@gmail.com>
2026-02-20 17:46:43 -0500

    Added sys.path to resolve services module import error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import pytest
 from sqlalchemy import event
 from app.db.session import engine, SessionLocal
